59ad856b98bc1fe01891fecd56827dee819e6293
Aditya Kumar <adityakumar14072003@gmail.com>
2026-04-30 14:36:40 +0530

    fix keras compatibility loading

--- download_model.py
+++ download_model.py
@@ -6,7 +6,7 @@ os.makedirs(MODEL_DIR, exist_ok=True)
 
 hf_hub_download(
     repo_id="neuronsbyisshu/brain-tumor-model",
-    filename="brain_tumor_clean.h5",
+    filename="brain_tumor_final_fixed.h5",
     local_dir="model",
     local_dir_use_symlinks=False   
 )
