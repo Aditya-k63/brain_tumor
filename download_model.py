e8b5be155b5a0b74e11ecbbb15ccba08c9b1fcae
Aditya Kumar <adityakumar14072003@gmail.com>
2026-04-30 14:41:06 +0530

    fix model download build issue

--- download_model.py
+++ download_model.py
@@ -4,11 +4,11 @@ import os
 MODEL_DIR = "model"
 os.makedirs(MODEL_DIR, exist_ok=True)
 
-hf_hub_download(
+model_path = hf_hub_download(
     repo_id="neuronsbyisshu/brain-tumor-model",
     filename="brain_tumor_final_fixed.h5",
-    local_dir="model",
-    local_dir_use_symlinks=False   
+    local_dir=MODEL_DIR,
+    local_dir_use_symlinks=False
 )
 
-print("Model downloaded successfully!")
+print(f"Model downloaded at: {model_path}")
